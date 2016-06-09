7c309de244074f65962a17ad700d261aa86e6ff9
sivaprakasams <sivaprakasams@gmail.com>
2016-06-09 12:51:00 +0530

    modified the dashboard logic

--- HRPortal/Controllers/DashboardController.cs
+++ HRPortal/Controllers/DashboardController.cs
@@ -1,8 +1,5 @@
 ﻿using HRPortal.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace HRPortal.Controllers
@@ -14,10 +11,10 @@ namespace HRPortal.Controllers
         public ActionResult Index()
         {
             HistoryViewModels obj = new HistoryViewModels();
-            var data = (from item in db.STATUS_HISTORY.Where(x => x.ISACTIVE == true).ToList()
-                        join stsMst in db.STATUS_MASTER on item.STATUS_ID equals stsMst.STATUS_ID
+            var data = (from item in db.CANDIDATES.Where(x => x.ISACTIVE == true).ToList()
+                        join stsMst in db.STATUS_MASTER on item.STATUS equals stsMst.STATUS_ID.ToString()
                         where stsMst.ISACTIVE == true
-                        select stsMst).ToList();
+                        select stsMst).ToList<STATUS_MASTER>();
             obj.ToT_Candidates_OFRD = data.Where(x => x.STATUS_NAME.Contains("OFFRD")).Count();
             obj.ToT_Candidates_PRGS = data.Where(x => !x.STATUS_NAME.Contains("OFFRD") && !x.STATUS_NAME.Contains("RJ")).Count();
             obj.ToT_Candidates_RJTD = data.Where(x => x.STATUS_NAME.Contains("RJ")).Count();
